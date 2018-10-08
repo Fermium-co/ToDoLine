7de13adbd9859ab9c0dc82f54aaf2f98d6dbc088
Marzie Sangtarash <m.sangtarash@outlook.com>
2018-10-08 16:36:27 +0330

    Add test for Updating and Deleting to do items

--- ToDoLine.Test/Controller/ToDoItemsControllerTest.cs
+++ ToDoLine.Test/Controller/ToDoItemsControllerTest.cs
@@ -95,5 +95,79 @@ namespace ToDoLine.Test.Controller
                 Assert.AreEqual(true, toDoItem.ShowInMyDay);
             }
         }
+
+        [TestMethod]
+        public async Task UpdateToDoItemTest()
+        {
+            using (ToDoLineTestEnv testEnv = new ToDoLineTestEnv())
+            {
+                IODataClient client = testEnv.Server.BuildODataClient(odataRouteName: "ToDoLine");
+
+                string userName = Guid.NewGuid().ToString("N");
+
+                await client.Controller<UserRegistrationController, UserRegistrationDto>()
+                    .Action(nameof(UserRegistrationController.Register))
+                    .Set(new UserRegistrationController.RegisterArgs { userRegistration = new UserRegistrationDto { UserName = userName, Password = "P@ssw0rd" } })
+                    .ExecuteAsync();
+
+                var token = await testEnv.Server.Login(userName, "P@ssw0rd", "ToDoLine", "secret");
+
+                client = testEnv.Server.BuildODataClient(token: token, odataRouteName: "ToDoLine");
+
+                ToDoGroupDto toDoGroup = await client.Controller<ToDoGroupsController, ToDoGroupDto>()
+                   .Action(nameof(ToDoGroupsController.CreateToDoGroup))
+                   .Set(new ToDoGroupsController.CreateToDoGroupArgs { title = "Test" })
+                   .ExecuteAsSingleAsync();
+
+                ToDoItemDto toDoItem = await client.Controller<ToDoItemsController, ToDoItemDto>()
+                   .Set(new ToDoItemDto { Title = "Task1", Notes = "Hi this is the first sample", ToDoGroupId = toDoGroup.Id })
+                   .InsertEntryAsync();
+
+                toDoItem.Title += "!";
+                toDoItem.IsCompleted = true;
+
+                ToDoItemDto updatedToDoItem = await client.Controller<ToDoItemsController, ToDoItemDto>()
+                   .Key(toDoItem.Id)
+                   .Set(toDoItem)
+                   .UpdateEntryAsync();
+
+                Assert.AreEqual("Task1!", updatedToDoItem.Title);
+                Assert.AreEqual(true, updatedToDoItem.IsCompleted);
+                Assert.AreEqual(userName, updatedToDoItem.CompletedBy);
+            }
+        }
+
+        [TestMethod]
+        public async Task DeleteToDoItemTest()
+        {
+            using (ToDoLineTestEnv testEnv = new ToDoLineTestEnv())
+            {
+                IODataClient client = testEnv.Server.BuildODataClient(odataRouteName: "ToDoLine");
+
+                string userName = Guid.NewGuid().ToString("N");
+
+                await client.Controller<UserRegistrationController, UserRegistrationDto>()
+                    .Action(nameof(UserRegistrationController.Register))
+                    .Set(new UserRegistrationController.RegisterArgs { userRegistration = new UserRegistrationDto { UserName = userName, Password = "P@ssw0rd" } })
+                    .ExecuteAsync();
+
+                var token = await testEnv.Server.Login(userName, "P@ssw0rd", "ToDoLine", "secret");
+
+                client = testEnv.Server.BuildODataClient(token: token, odataRouteName: "ToDoLine");
+
+                ToDoGroupDto toDoGroup = await client.Controller<ToDoGroupsController, ToDoGroupDto>()
+                   .Action(nameof(ToDoGroupsController.CreateToDoGroup))
+                   .Set(new ToDoGroupsController.CreateToDoGroupArgs { title = "Test" })
+                   .ExecuteAsSingleAsync();
+
+                ToDoItemDto toDoItem = await client.Controller<ToDoItemsController, ToDoItemDto>()
+                   .Set(new ToDoItemDto { Title = "Task1", Notes = "Hi this is the first sample", ToDoGroupId = toDoGroup.Id })
+                   .InsertEntryAsync();
+
+                await client.Controller<ToDoItemsController, ToDoItemDto>()
+                    .Key(toDoItem.Id)
+                    .DeleteEntryAsync();
+            }
+        }
     }
 }
