35258c22bba41ad44bd1b0f9a2caa93d2b2e6712
Yaser Moradi <ysmoradi@outlook.com>
2019-01-19 09:13:30 +0330

    Added clean.bat file

--- Clean.bat
+++ Clean.bat
@@ -1,22 +1,10 @@
-@ECHO off
-cls
-
-ECHO Deleting all BIN and OBJ & .vs folders...
-ECHO.
-
-rd /s/q .vs
-
-FOR /d /r . %%d in (bin,obj) DO (
-	IF EXIST "%%d" (		 	 
-		ECHO %%d | FIND /I "\node_modules\" > Nul && ( 
-			ECHO.Skipping: %%d
-		) || (
-			ECHO.Deleting: %%d
-			rd /s/q "%%d"
-		)
-	)
-)
-
-ECHO.
-ECHO. Press any key to exit.
-pause > nul
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S node_modules') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S Packages') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S .vs') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S TestResults') DO RMDIR /S /Q "%%G"
+DEL /Q /F /S "Resource.designer.cs"
+DEL /Q /F /S "*.csproj.user"
+DEL /Q /F /S "*.Model.Context.d.ts"
+DEL /Q /F /S "*.Model.Context.js"
