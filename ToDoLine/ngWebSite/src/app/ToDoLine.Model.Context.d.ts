772e259bf78326523166f08ea2858efe7d9b3a19
Yaser Moradi <ysmoradi@outlook.com>
2019-01-20 20:19:20 +0330

    Moved WebSite to ngWebSite folder

---
+++ ToDoLine/ngWebSite/src/app/ToDoLine.Model.Context.d.ts
@@ -0,0 +1,194 @@
+﻿/// <reference path="../../node_modules/@bit/jaydata/jaydata.d.ts" />
+
+declare module ToDoLine.Dto {
+	
+	class ToDoItemStepDto extends $data.Entity {
+				    
+			Id : string;
+			static Id : any;
+				    
+			Text : string;
+			static Text : any;
+				    
+			ToDoItemId : string;
+			static ToDoItemId : any;
+				    
+			IsCompleted : boolean;
+			static IsCompleted : any;
+			}
+}
+
+
+declare module ToDoLine.Dto {
+	
+	class ToDoGroupDto extends $data.Entity {
+				    
+			Id : string;
+			static Id : any;
+				    
+			Title : string;
+			static Title : any;
+				    
+			CreatedBy : string;
+			static CreatedBy : any;
+				    
+			CreatedOn : Date;
+			static CreatedOn : any;
+				    
+			ModifiedOn : Date;
+			static ModifiedOn : any;
+				    
+			Theme : string;
+			static Theme : any;
+				    
+			SortedBy : ToDoLine.Enum.SortBy;
+			static SortedBy : any;
+				    
+			HideCompletedToDoItems : boolean;
+			static HideCompletedToDoItems : any;
+				    
+			SharedByCount : number;
+			static SharedByCount : any;
+			}
+}
+
+
+declare module ToDoLine.Dto {
+	
+	class UserRegistrationDto extends $data.Entity {
+				    
+			UserName : string;
+			static UserName : any;
+				    
+			Password : string;
+			static Password : any;
+			}
+}
+
+
+declare module ToDoLine.Dto {
+	
+	class UserDto extends $data.Entity {
+				    
+			Id : string;
+			static Id : any;
+				    
+			UserName : string;
+			static UserName : any;
+			}
+}
+
+
+declare module ToDoLine.Dto {
+	
+	class ToDoItemDto extends $data.Entity {
+				    
+			Id : string;
+			static Id : any;
+				    
+			Title : string;
+			static Title : any;
+				    
+			IsImportant : boolean;
+			static IsImportant : any;
+				    
+			IsCompleted : boolean;
+			static IsCompleted : any;
+				    
+			Notes : string;
+			static Notes : any;
+				    
+			DueDate : Date;
+			static DueDate : any;
+				    
+			CreatedBy : string;
+			static CreatedBy : any;
+				    
+			CompletedBy : string;
+			static CompletedBy : any;
+				    
+			CreatedOn : Date;
+			static CreatedOn : any;
+				    
+			ModifiedOn : Date;
+			static ModifiedOn : any;
+				    
+			RemindOn : Date;
+			static RemindOn : any;
+				    
+			ShowInMyDay : boolean;
+			static ShowInMyDay : any;
+				    
+			ToDoGroupId : string;
+			static ToDoGroupId : any;
+				    
+			ToDoItemStepsCount : number;
+			static ToDoItemStepsCount : any;
+				    
+			ToDoItemStepsCompletedCount : number;
+			static ToDoItemStepsCompletedCount : any;
+			}
+}
+
+
+
+declare module ToDoLine.Enum {
+	
+	enum SortBy {
+				    /**Value: 0*/
+			CreatedDate,
+				    /**Value: 1*/
+			Importance,
+			}
+}
+
+
+
+    
+	interface ToDoItemsEntitySet extends $data.EntitySet<ToDoLine.Dto.ToDoItemDto>{
+				    
+		    getMyToDoItems():  $data.Queryable<ToDoLine.Dto.ToDoItemDto> ;
+			}
+    
+	interface UsersEntitySet extends $data.EntitySet<ToDoLine.Dto.UserDto>{
+				    
+		    getAllUsers():  $data.Queryable<ToDoLine.Dto.UserDto> ;
+			}
+    
+	interface UserRegistrationEntitySet extends $data.EntitySet<ToDoLine.Dto.UserRegistrationDto>{
+				    
+		    register(userRegistration : ToDoLine.Dto.UserRegistrationDto):  Promise<void> ;
+			}
+    
+	interface ToDoGroupsEntitySet extends $data.EntitySet<ToDoLine.Dto.ToDoGroupDto>{
+				    
+		    getMyToDoGroups():  $data.Queryable<ToDoLine.Dto.ToDoGroupDto> ;
+				    
+		    createToDoGroup(title : string):  Promise<ToDoLine.Dto.ToDoGroupDto> ;
+				    
+		    shareToDoGroupWithAnotherUser(toDoGroupId : string,anotherUserId : string):  Promise<ToDoLine.Dto.ToDoGroupDto> ;
+				    
+		    kickUserFromToDoGroup(userId : string,toDoGroupId : string):  Promise<void> ;
+			}
+    
+	interface ToDoItemStepsEntitySet extends $data.EntitySet<ToDoLine.Dto.ToDoItemStepDto>{
+				    
+		    getToDoItemSteps(toDoItemId : string):  $data.Queryable<ToDoLine.Dto.ToDoItemStepDto> ;
+			}
+
+declare class ToDoLineContext extends $data.EntityContext {
+
+		    
+		toDoItems: ToDoItemsEntitySet;
+		    
+		users: UsersEntitySet;
+		    
+		userRegistration: UserRegistrationEntitySet;
+		    
+		toDoGroups: ToDoGroupsEntitySet;
+		    
+		toDoItemSteps: ToDoItemStepsEntitySet;
+	
+}
+
+
---
+++ ToDoLine/ngWebSite/src/app/ToDoLine.Model.Context.js
@@ -0,0 +1,327 @@
+﻿
+
+		 var   ToDoLine = ToDoLine || {};
+		 ToDoLine.Dto = ToDoLine.Dto || {};
+
+ToDoLine.Dto.ToDoItemStepDto = $data.Entity.extend("ToDoLine.Dto.ToDoItemStepDto", {
+	 
+		Id: {
+			"type": "Edm.Guid" , nullable: false
+															, "key": true
+			, "required" : true
+			, "computed": true
+										},
+	 
+		Text: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		ToDoItemId: {
+			"type": "Edm.Guid" , nullable: false
+																			},
+	 
+		IsCompleted: {
+			"type": "Edm.Boolean" , nullable: false
+																			},
+		});
+
+
+		 var   ToDoLine = ToDoLine || {};
+		 ToDoLine.Dto = ToDoLine.Dto || {};
+
+ToDoLine.Dto.ToDoGroupDto = $data.Entity.extend("ToDoLine.Dto.ToDoGroupDto", {
+	 
+		Id: {
+			"type": "Edm.Guid" , nullable: false
+															, "key": true
+			, "required" : true
+			, "computed": true
+										},
+	 
+		Title: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		CreatedBy: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		CreatedOn: {
+			"type": "Edm.DateTimeOffset" , nullable: false
+																			},
+	 
+		ModifiedOn: {
+			"type": "Edm.DateTimeOffset" , nullable: false
+																			},
+	 
+		Theme: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		SortedBy: {
+			"type": "ToDoLine.Enum.SortBy" , nullable: false
+																			},
+	 
+		HideCompletedToDoItems: {
+			"type": "Edm.Boolean" , nullable: false
+																			},
+	 
+		SharedByCount: {
+			"type": "Edm.Int32" , nullable: false
+																			},
+		});
+
+
+		 var   ToDoLine = ToDoLine || {};
+		 ToDoLine.Dto = ToDoLine.Dto || {};
+
+ToDoLine.Dto.UserRegistrationDto = $data.Entity.extend("ToDoLine.Dto.UserRegistrationDto", {
+	 
+		UserName: {
+			"type": "Edm.String" , nullable: true
+															, "key": true
+			, "required" : false
+			, "computed": true
+										},
+	 
+		Password: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+		});
+
+
+		 var   ToDoLine = ToDoLine || {};
+		 ToDoLine.Dto = ToDoLine.Dto || {};
+
+ToDoLine.Dto.UserDto = $data.Entity.extend("ToDoLine.Dto.UserDto", {
+	 
+		Id: {
+			"type": "Edm.Guid" , nullable: false
+															, "key": true
+			, "required" : true
+			, "computed": true
+										},
+	 
+		UserName: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+		});
+
+
+		 var   ToDoLine = ToDoLine || {};
+		 ToDoLine.Dto = ToDoLine.Dto || {};
+
+ToDoLine.Dto.ToDoItemDto = $data.Entity.extend("ToDoLine.Dto.ToDoItemDto", {
+	 
+		Id: {
+			"type": "Edm.Guid" , nullable: false
+															, "key": true
+			, "required" : true
+			, "computed": true
+										},
+	 
+		Title: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		IsImportant: {
+			"type": "Edm.Boolean" , nullable: false
+																			},
+	 
+		IsCompleted: {
+			"type": "Edm.Boolean" , nullable: false
+																			},
+	 
+		Notes: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		DueDate: {
+			"type": "Edm.DateTimeOffset" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		CreatedBy: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		CompletedBy: {
+			"type": "Edm.String" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		CreatedOn: {
+			"type": "Edm.DateTimeOffset" , nullable: false
+																			},
+	 
+		ModifiedOn: {
+			"type": "Edm.DateTimeOffset" , nullable: false
+																			},
+	 
+		RemindOn: {
+			"type": "Edm.DateTimeOffset" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		ShowInMyDay: {
+			"type": "Edm.Boolean" , nullable: false
+																			},
+	 
+		ToDoGroupId: {
+			"type": "Edm.Guid" , nullable: true
+			  , defaultValue: null
+																			},
+	 
+		ToDoItemStepsCount: {
+			"type": "Edm.Int32" , nullable: false
+																			},
+	 
+		ToDoItemStepsCompletedCount: {
+			"type": "Edm.Int32" , nullable: false
+																			},
+		});
+
+
+
+		 var   ToDoLine = ToDoLine || {};
+		 ToDoLine.Enum = ToDoLine.Enum || {};
+
+ToDoLine.Enum.SortBy = $data.createEnum("ToDoLine.Enum.SortBy", null, $data.String, [
+	 
+		{ name : 'CreatedDate' , value : "ToDoLine.Enum.SortBy'CreatedDate'" , index : 0 },
+	 
+		{ name : 'Importance' , value : "ToDoLine.Enum.SortBy'Importance'" , index : 1 },
+		]);
+
+
+ToDoLineContext = $data.EntityContext.extend("ToDoLineContext", {
+			toDoItems : {
+			"type": "$data.EntitySet",
+			"elementType": "ToDoLine.Dto.ToDoItemDto",
+							"actions": {
+													"getMyToDoItems": {
+								"type": "$data.ServiceFunction",
+								"namespace": "Default",
+								"returnType":  "$data.Queryable" ,
+								 "elementType": "ToDoLine.Dto.ToDoItemDto", 
+																	"params": [
+																			]
+						},
+										}
+					},
+			users : {
+			"type": "$data.EntitySet",
+			"elementType": "ToDoLine.Dto.UserDto",
+							"actions": {
+													"getAllUsers": {
+								"type": "$data.ServiceFunction",
+								"namespace": "Default",
+								"returnType":  "$data.Queryable" ,
+								 "elementType": "ToDoLine.Dto.UserDto", 
+																	"params": [
+																			]
+						},
+										}
+					},
+			userRegistration : {
+			"type": "$data.EntitySet",
+			"elementType": "ToDoLine.Dto.UserRegistrationDto",
+							"actions": {
+													"register": {
+								"type": "$data.ServiceAction",
+								"namespace": "Default",
+								"returnType":  null ,
+																	"params": [
+																					{
+												"name": "userRegistration",
+												"type": "ToDoLine.Dto.UserRegistrationDto",
+																							},									
+																			]
+						},
+										}
+					},
+			toDoGroups : {
+			"type": "$data.EntitySet",
+			"elementType": "ToDoLine.Dto.ToDoGroupDto",
+							"actions": {
+													"getMyToDoGroups": {
+								"type": "$data.ServiceFunction",
+								"namespace": "Default",
+								"returnType":  "$data.Queryable" ,
+								 "elementType": "ToDoLine.Dto.ToDoGroupDto", 
+																	"params": [
+																			]
+						},
+													"createToDoGroup": {
+								"type": "$data.ServiceAction",
+								"namespace": "Default",
+								"returnType":  "ToDoLine.Dto.ToDoGroupDto" ,
+																	"params": [
+																					{
+												"name": "title",
+												"type": "Edm.String",
+																							},									
+																			]
+						},
+													"shareToDoGroupWithAnotherUser": {
+								"type": "$data.ServiceAction",
+								"namespace": "Default",
+								"returnType":  "ToDoLine.Dto.ToDoGroupDto" ,
+																	"params": [
+																					{
+												"name": "toDoGroupId",
+												"type": "Edm.Guid",
+																							},									
+																					{
+												"name": "anotherUserId",
+												"type": "Edm.Guid",
+																							},									
+																			]
+						},
+													"kickUserFromToDoGroup": {
+								"type": "$data.ServiceAction",
+								"namespace": "Default",
+								"returnType":  null ,
+																	"params": [
+																					{
+												"name": "userId",
+												"type": "Edm.Guid",
+																							},									
+																					{
+												"name": "toDoGroupId",
+												"type": "Edm.Guid",
+																							},									
+																			]
+						},
+										}
+					},
+			toDoItemSteps : {
+			"type": "$data.EntitySet",
+			"elementType": "ToDoLine.Dto.ToDoItemStepDto",
+							"actions": {
+													"getToDoItemSteps": {
+								"type": "$data.ServiceFunction",
+								"namespace": "Default",
+								"returnType":  "$data.Queryable" ,
+								 "elementType": "ToDoLine.Dto.ToDoItemStepDto", 
+																	"params": [
+																					{
+												"name": "toDoItemId",
+												"type": "Edm.Guid",
+																							},									
+																			]
+						},
+										}
+					},
+	});
+
+
