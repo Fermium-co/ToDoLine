95f4392189514103f166ac3c78785f2a04e818a7
msangtarash <m.sangtarash@outlook.com>
2019-01-28 19:32:23 +0330

    Add image and strings

--- App/ToDoLineApp/Resources/ImageNames.cs
+++ App/ToDoLineApp/Resources/ImageNames.cs
@@ -12,5 +12,7 @@
         public static string Task = nameof(Task);
         public static string Trash = nameof(Trash);
         public static string Edit = nameof(Edit);
+        public static string GrayStar = nameof(GrayStar);
+        public static string FillStar = nameof(FillStar);
     }
 }
